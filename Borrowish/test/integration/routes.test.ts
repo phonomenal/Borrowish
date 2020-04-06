3b461ee5dd2b28801d2e7302787864af023dfe02
Ivan Zhang <ivanzhangemail@gmail.com>
2020-04-06 09:12:50 -0700

    Added sample api and unit tests

---
+++ Borrowish/test/integration/routes.test.ts
@@ -0,0 +1,19 @@
+import chai from "chai";
+import app from "../../server/server";
+import chaiHttp from "chai-http";
+import "mocha";
+
+chai.use(chaiHttp);
+const expect = chai.expect;
+
+describe("Hello API request", () => {
+  it("show return with a response on call", () => {
+    return chai
+      .request(app)
+      .get("/api/hello")
+      .then((res) => {
+        expect(res.status).to.eql(200);
+        expect(res.body.express).to.contain("Hello From Node Js Service");
+      });
+  });
+});
