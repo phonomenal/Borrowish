7371e51c79fbf576244d549a5d7c0314c49c7850
Ivan Zhang <ivanzhangemail@gmail.com>
2020-04-08 08:30:48 -0700

    Testing heroku deployment

--- Borrowish/server/server.ts
+++ Borrowish/server/server.ts
@@ -1,5 +1,6 @@
-var express = require("express");
-var bodyParser = require("body-parser");
+import express from "express";
+import bodyParser from "body-parser";
+import { Request, Response } from "express";
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -7,11 +8,11 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/api/hello", (req: any, res: any) => {
+app.get("/api/hello", (req: Request, res: Response) => {
   res.send({ express: "Hello From Node Js Service" });
 });
 
-app.post("/api/world", (req: any, res: any) => {
+app.post("/api/world", (req: Request, res: Response) => {
   console.log(req.body);
   res.send(
     `I received your POST request. This is what you sent me: ${req.body.post}`
