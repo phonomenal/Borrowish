fb30823bb3bf59f37bf6eb1445f0890c4ab8c608
Ivan Zhang <ivanzhangemail@gmail.com>
2020-04-09 13:47:32 -0700

    Adding the server calls before the static one

--- server/server.ts
+++ server/server.ts
@@ -9,16 +9,6 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// Serve static assets if in prod env
-if (process.env.NODE_ENV === "production") {
-  // Set static folder
-  app.use(express.static("client/build"));
-
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
-}
-
 app.get("/api/hello", (req: Request, res: Response) => {
   res.send({ express: "Hello From Node Js Service!" });
 });
@@ -29,6 +19,16 @@ app.post("/api/world", (req: Request, res: Response) => {
     `I received your POST request. This is what you sent me: ${req.body.post}`
   );
 });
+
+// Serve static assets if in prod env
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
