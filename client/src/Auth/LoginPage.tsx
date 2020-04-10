e134b0d432fc1473361c3deb1ff1df4f5dda71a7
James-LeHa <jlhacba@outlook.com>
2020-04-09 23:57:26 -0500

    Add login page onChange Event handling

--- client/src/Auth/LoginPage.tsx
+++ client/src/Auth/LoginPage.tsx
@@ -1,12 +1,25 @@
 import React from 'react';
+import { useState } from 'react';
 
 const LoginPage = () => {
+
+    const [{username, password}, setCredentials] = useState({
+        username: '',
+        password: ''
+    })
+
     return (
         <form>
             <label htmlFor="username">Username</label>
-            <input placeholder="Username" />
+            <input placeholder="Username" value={username} onChange={(event) => setCredentials({
+                username: event.target.value,
+                password
+            }) } />
             <label htmlFor="password">Password</label>
-            <input placeholder="Password" type='password' />
+            <input placeholder="Password" type='password' value={password} onChange={(event) => setCredentials({
+                username,
+                password: event.target.value,
+            }) } />
             <button type='submit'>Login</button>
         </form>
     )
